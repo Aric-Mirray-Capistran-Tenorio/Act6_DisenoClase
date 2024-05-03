5d07800bc9bc0743c3ed74e805dc4a1808de6854
ARIC_MIRRAY_CAPISTRAN_TENORIO <a.21308051280442@cbtis128.edu.mx>
2024-05-03 17:04:37 +0000

    nuevo

---
+++ lib/data/product_data.dart
@@ -0,0 +1,37 @@
+import 'package:capistran0442/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+    img: "assets/albanil.jpg",
+    id: "id: 1",
+    nombre: "nombre: Cuchara para Cemento",
+    precio: "precio: 160",
+    modelo: "modelo: t65",
+    marca: "marca: TRUPER",
+    color: "color: Naranja",
+    region: "region: Mexico",
+    categoria: "categoria: Abañileria",
+  ),
+  ProductModel(
+    img: "assets/carpinteria.jpg",
+    id: "id: 2",
+    nombre: "nombre: Cuchara para Cemento",
+    precio: "precio: 160",
+    modelo: "modelo: t65",
+    marca: "marca: TRUPER",
+    color: "color: Naranja",
+    region: "region: Mexico",
+    categoria: "categoria: Carpinteria",
+  ),
+  ProductModel(
+    img: "assets/jardineria.jpg",
+    id: "id: 3",
+    nombre: "nombre Cuchara para Cemento",
+    precio: "precio: 160",
+    modelo: "modelo: t65",
+    marca: "marca: TRUPER",
+    color: "color: Naranja",
+    region: "region: Mexico",
+    categoria: "categoria: Jardineria",
+  ),
+];
